e725d224348d27a4c70b3483f3039fe1fc2cc7a7
Sebastian <sebastiansloma@gmail.com>
2023-02-15 12:15:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convBtn = document.querySelector('.conv');
+const resetBtn = document.querySelector('.reset');
+const changeBtn = document.querySelector('.change');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+
+let fahrenheit;
+let celsius;
+
+const swap = () => {
+	if (one.innerText === '°C') {
+		one.innerText = '°F';
+		two.innerText = '°C';
+	} else {
+		one.innerText = '°C';
+		two.innerText = '°F';
+	}
+};
+
+changeBtn.addEventListener('click', swap)
