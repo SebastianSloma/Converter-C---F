15900d45a000015156a11d5555a86b2edbb6ac09
Sebastian <sebastiansloma@gmail.com>
2023-02-15 12:49:05 +0100

    added js

--- script.js
+++ script.js
@@ -13,10 +13,43 @@ const swap = () => {
 	if (one.innerText === '°C') {
 		one.innerText = '°F';
 		two.innerText = '°C';
+		result.innerText = '';
 	} else {
 		one.innerText = '°C';
 		two.innerText = '°F';
+		result.innerText = '';
 	}
 };
 
-changeBtn.addEventListener('click', swap)
+const celToFahr = () => {
+	fahrenheit = converter.value * 1.8 + 32;
+	result.innerText = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
+	converter.value = '';
+};
+
+const fahrToCel = () => {
+	celsius = (converter.value - 32) / 1.8;
+	result.innerText = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
+	converter.value = '';
+};
+
+const conversion = () => {
+	if (converter.value !== '') {
+		if (one.innerText === '°C') {
+			celToFahr();
+		} else {
+			fahrToCel();
+		}
+	} else {
+		result.innerText = 'You need to enter a number!';
+	}
+};
+
+const reset = () => {
+	converter.value = '';
+	result.innerText = '';
+};
+
+convBtn.addEventListener('click', conversion);
+changeBtn.addEventListener('click', swap);
+resetBtn.addEventListener('click', reset);
